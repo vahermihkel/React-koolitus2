61016a39168bf2a422866287eec9e33b87e1f519
mihkelvaher <mihkel.vaher@industry62.com>
2023-04-20 12:05:12 +0300

    usestate, useref, dünaamiline väljakuvamine

--- veebipood/src/pages/Avaleht.js
+++ veebipood/src/pages/Avaleht.js
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Avaleht() {
+  const [kogus, uuendaKogus] = useState(12);
+  const [laigitud, uuendaLaigitud] = useState(false); // 0
+  const [sonum, uuendaSonum] = useState("Uuenda kogust!");
+
+  function nulli() {
+    uuendaKogus(0);
+    uuendaSonum("Panid tagasi nulli!");
+  }
+
+  function vahenda() {
+    uuendaKogus(kogus - 1);
+    uuendaSonum("Vähendasid kogust!");
+  }
+
+  function suurenda() {
+    uuendaKogus(kogus + 1);
+    uuendaSonum("Suurendasid kogust!");
+  }
+
   return (
-    <div>Avaleht</div>
+    <div>
+      {laigitud === true && <img src="/laigitud.svg" alt="" />}
+      {laigitud === false && <img src="/mittelaigitud.svg" alt="" />}
+      <span>{laigitud + 0}</span>
+      <button onClick={() => uuendaLaigitud(true)}>Muuda laigituks</button>
+      <button onClick={() => uuendaLaigitud(false)}>Muuda mittelaigituks</button>
+
+      <br /><br />
+
+      <div>{sonum}</div>
+      {kogus !== 0 && <button onClick={nulli}>Tagasi nulli</button>}
+      <button onClick={vahenda}>-</button>
+      <span>kogus: {kogus} tükki</span>
+      <button onClick={suurenda}>+</button>
+    </div>
   )
 }
 
--- veebipood/src/pages/LisaToode.js
+++ veebipood/src/pages/LisaToode.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
 function LisaToode() {
+  const [sonum, uuendaSonum] = useState("Lisa toode!");
+  const inputiLuger = useRef(); // ref -- loeb inputi seest väärtusi
+  // const nimiRef = useRef();
+  // const nimiViide = useRef();
+
+  // function lisa() {}
+
+  const lisa = () => {
+    uuendaSonum("Toode lisatud, nimega: " + inputiLuger.current.value);
+  }
+
   return (
-    <div>LisaToode</div>
+    <div>
+      <div>{sonum}</div>
+      <label>Uue toote nimi</label> <br />
+      <input ref={inputiLuger} type="text" /> <br />
+      <button onClick={lisa}>Lisa</button> <br />
+    </div>
   )
 }
 
